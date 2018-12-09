7403dcb92c8f0ef60e93635faa716cb165eb22df
XiaozhenChen <littlepearl@hotmail.com>
2018-12-09 13:42:17 +0800

    GLCall macro to get error of gl function for debug

--- Application.cpp
+++ Application.cpp
@@ -2,9 +2,29 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include <fstream>
-#include <string>
+#include <string>6
 #include <sstream>
 
+#define ASSERT(x) if (!(x)) __debugbreak();
+#define GLCall(x) GLClearError();\
+     x;\
+     ASSERT(GLLogCall(#x,__FILE__,__LINE__))
+
+static void GLClearError()
+{
+	while (glGetError() != GL_NO_ERROR);
+}
+
+static bool GLLogCall(const char* function, const char* file, int line)
+{
+	while (GLenum error = glGetError())
+	{
+		std::cout << "[Opengl Error] (" << error <<")"<< function<<
+			" "<<file<<":"<<line<< std::endl;
+		return false;
+	}
+	return true;
+}
 struct ShaderProgramSource
 {
 	std::string VertexSource;
@@ -178,8 +198,10 @@ int main(void)
 		*/
 		
 	//	glDrawArrays(GL_TRIANGLES,0,6);// change the points number to braw
-		glDrawElements(GL_TRIANGLES, 6,GL_UNSIGNED_INT,nullptr);
+		GLCall(glDrawElements(GL_TRIANGLES, 6,GL_UNSIGNED_INT,nullptr));
+				
 	/* Swap front and back buffers */
+
 		glfwSwapBuffers(window);
 
 		/* Poll for and process events */
