7980c48a2d5f45f13ffc7574a209bab4e00648a5
XiaozhenChen <littlepearl@hotmail.com>
2018-12-14 23:28:57 +0800

    debug link error

--- Application.cpp
+++ Application.cpp
@@ -6,6 +6,8 @@
 #include <sstream>
 
 #include "Renderer.h"
+#include "VertexBuffer.h"
+#include "IndexBuffer.h"
 
 struct ShaderProgramSource
 {
@@ -150,28 +152,15 @@ int main(void)
 	GLCall(glGenVertexArrays(1, &vao));
 	GLCall(glBindVertexArray(vao));
 
-
-	unsigned int buffer; //opengl state machine, 每个产生的东西都有对应的地址
-	glGenBuffers(1, &buffer); //所以需要绑定
-	glBindBuffer(GL_ARRAY_BUFFER, buffer);//绑定一个容器
-	glBufferData(GL_ARRAY_BUFFER,4*2*sizeof(float),position,GL_STATIC_DRAW);                                  //声明这个容器的大小
-
-	//why must unsigned?
-	unsigned int ibo; //opengl state machine, 每个产生的东西都有对应的地址
-	glGenBuffers(1, &ibo); //所以需要绑定
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);//绑定一个容器
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6  * sizeof(unsigned int), indices, GL_STATIC_DRAW);                                  //声明这个容器的大小
-
+	VertexBuffer vb(position, 4 * 2 * sizeof(float));//data size
 
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0,2,GL_FLOAT,GL_FALSE,sizeof(float)*2,0);
 
+	IndexBuffer ib(indices, 6);//data count
+
 	ShaderProgramSource source = ParseShader("Basic.shader");  // notice the file path
-	//文件夹后面才有 ‘/’ 这个是错误的  /res/shaders/Basic.shader 读不到
-	std::cout << "vertex" << std::endl;
-	//std::cout << source.VertexSource<< std::endl;
-	std::cout << "fragment" << std::endl;
-	//std::cout << source.FragmentSource << std::endl;
+	
 
 	unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
 	GLCall(glUseProgram(shader));
@@ -182,9 +171,7 @@ int main(void)
 	
 	GLCall(glBindVertexArray(0));
 	GLCall(glUseProgram(0));
-	GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
-	GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
-
+	
 
 	float r = 0.0f;
 	float increment = 0.05f;
@@ -206,14 +193,16 @@ int main(void)
 		GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
 		
 		GLCall(glBindVertexArray(vao));
-		GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
-		
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+		ib.Bind();
+		//GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
+		GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+
+	//	glEnableVertexAttribArray(0);
+		//glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 
 		
 	//	glDrawArrays(GL_TRIANGLES,0,6);// change the points number to braw
-		GLCall(glDrawElements(GL_TRIANGLES, 6,GL_UNSIGNED_INT,nullptr));
+	//	GLCall(glDrawElements(GL_TRIANGLES, 6,GL_UNSIGNED_INT,nullptr));
 		if (r > 1.0f)
 			increment = -0.05f;
 		else if (r < 0.0f)
