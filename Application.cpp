1cd1c7ef2b3551cdd89105772011e4fbafe76430
XiaozhenChen <littlepearl@hotmail.com>
2018-12-23 00:07:14 +0800

    set up Test Framework

--- Application.cpp
+++ Application.cpp
@@ -21,6 +21,7 @@
 #include "src\vendor\imgui\imgui.h"
 #include "src\vendor\imgui\imgui_impl_glfw_gl3.h"
 
+#include "Project1\src\test\TestClearCode.h"
 int main(void)
 {
 	GLFWwindow* window;
@@ -49,60 +50,11 @@ int main(void)
 	
 	std::cout << glGetString(GL_VERSION) << std::endl;
 	
-		float positions[] = {
-			-50.0f,-50.0f, 0.0f,0.0f,
-			50.0f,-50.0f, 1.0f,0.0f,
-			50.0f, 50.0f, 1.0f,1.0f,
-			-50.0f,50.0f,  0.0f,1.0f
-
-		};
-	
-
-
-
-	unsigned int indices[] = {
-		0,1,2,
-		2,3,0
-	};
+		
 
 	GLCall(glEnable(GL_BLEND));
 	GLCall(glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA));
 
-	//unsigned int vao;
-	//GLCall(glGenVertexArrays(1, &vao));
-	//GLCall(glBindVertexArray(vao));
-
-	VertexArray va;
-
-	VertexBuffer vb(positions, 4 * 4 * sizeof(float));//data size
-	
-	VertexBufferLayout layout;
-	layout.Push<float>(2);
-	layout.Push<float>(2);
-	va.AddBuffer(vb, layout);
-	
-	IndexBuffer ib(indices,6);//data count
-
-	glm::mat4 proj = glm::ortho(0.0f,960.0f,0.0f,540.0f,-1.0f,1.0f);
-	glm::mat4 view = glm::translate(glm::mat4(1.0f) , glm::vec3(0, 0, 0));
-	
-	Shader shader("Basic.shader");
-
-
-	shader.Bind();
-	shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
-
-	
-
-	Texture texture("res/texture/cxz.png");
-	texture.Bind();
-
-	shader.SetUniform1i("u_Texture", 0);
-
-	va.UnBind();
-	vb.UnBind();
-	ib.UnBind();
-		shader.Unbind();
 	
 	
 		Renderer renderer;
@@ -111,85 +63,25 @@ int main(void)
 		ImGui_ImplGlfwGL3_Init(window, true);
 		ImGui::StyleColorsDark();
 
-		glm::vec3 translationA(200, 200, 0);
-		glm::vec3 translationB(400, 200, 0);
+		test::TestClearColor test;
 
-		//bool show_demo_window = true;
-		//bool show_another_window = false;
-		//ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-
-
-	float r = 0.0f;
-	float increment = 0.05f;
-	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window)) 
 	{
 		renderer.Clear();
 
+		test.OnUpdate(0.0f);
+		test.OnRender();
+
+
 		ImGui_ImplGlfwGL3_NewFrame();
 
-
-		
-
-		{
-	      
-			glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
-	    	glm::mat4 mvp = proj * view * model;
-			shader.Bind();
-			shader.SetUniformMat4f("u_MVP", mvp);
-			
-			renderer.Draw(va, ib, shader);
-		}
-	
-		{
-			glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
-			glm::mat4 mvp = proj * view * model;
-			shader.Bind();
-			shader.SetUniformMat4f("u_MVP", mvp);
-
-			renderer.Draw(va, ib, shader);
-		}
-
-
-
-	//	shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
-	
-	
-		
-
-	
-
-	//	glEnableVertexAttribArray(0);
-		//glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
-
-		
-	//	glDrawArrays(GL_TRIANGLES,0,6);// change the points number to braw
-	//	GLCall(glDrawElements(GL_TRIANGLES, 6,GL_UNSIGNED_INT,nullptr));
-		if (r > 1.0f)
-			increment = -0.05f;
-		else if (r < 0.0f)
-			increment = 0.05f;
-		r += increment;
-
-		{
-		//	static float f = 0.0f;
-		//	static int counter = 0;
-		//	ImGui::Text("Hello, world!");                           // Display some text (you can use a format string too)
-			ImGui::SliderFloat3("Translation A", &translationA.x, 0.0f, 960.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
-			ImGui::SliderFloat3("Translation B", &translationB.x, 0.0f, 960.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
-
-
-			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-		
-
-		
-		}
+		test.OnImGuiRender();
 
 		ImGui::Render();
 		ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
 
 
-	/* Swap front and back buffers */
+
 
 		glfwSwapBuffers(window);
 
