d18602e27c3a27fbee368e87ee525fbcbe1d81fd
XiaozhenChen <littlepearl@hotmail.com>
2018-12-22 23:17:16 +0800

    render two object use the same Texture

--- Application.cpp
+++ Application.cpp
@@ -50,10 +50,10 @@ int main(void)
 	std::cout << glGetString(GL_VERSION) << std::endl;
 	
 		float positions[] = {
-			100.0f,100.0f,0.0f,0.0f,
-			200.0f,100.0f,1.0f,0.0f,
-			200.0f, 200.0f,1.0f,1.0f,
-			100.0f,200.0f,0.0f,1.0f
+			-50.0f,-50.0f, 0.0f,0.0f,
+			50.0f,-50.0f, 1.0f,0.0f,
+			50.0f, 50.0f, 1.0f,1.0f,
+			-50.0f,50.0f,  0.0f,1.0f
 
 		};
 	
@@ -84,7 +84,7 @@ int main(void)
 	IndexBuffer ib(indices,6);//data count
 
 	glm::mat4 proj = glm::ortho(0.0f,960.0f,0.0f,540.0f,-1.0f,1.0f);
-	glm::mat4 view = glm::translate(glm::mat4(1.0f) , glm::vec3(-100, 0, 0));
+	glm::mat4 view = glm::translate(glm::mat4(1.0f) , glm::vec3(0, 0, 0));
 	
 	Shader shader("Basic.shader");
 
@@ -111,7 +111,8 @@ int main(void)
 		ImGui_ImplGlfwGL3_Init(window, true);
 		ImGui::StyleColorsDark();
 
-		glm::vec3 translation(200, 200, 0);
+		glm::vec3 translationA(200, 200, 0);
+		glm::vec3 translationB(400, 200, 0);
 
 		//bool show_demo_window = true;
 		//bool show_another_window = false;
@@ -127,18 +128,36 @@ int main(void)
 
 		ImGui_ImplGlfwGL3_NewFrame();
 
-		glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
 
-		glm::mat4 mvp = proj * view * model;
-
-
-
-		shader.Bind();
-		shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
 		
-		shader.SetUniformMat4f("u_MVP", mvp);
+
+		{
+	      
+			glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
+	    	glm::mat4 mvp = proj * view * model;
+			shader.Bind();
+			shader.SetUniformMat4f("u_MVP", mvp);
+			
+			renderer.Draw(va, ib, shader);
+		}
+	
+		{
+			glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
+			glm::mat4 mvp = proj * view * model;
+			shader.Bind();
+			shader.SetUniformMat4f("u_MVP", mvp);
+
+			renderer.Draw(va, ib, shader);
+		}
+
+
+
+	//	shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
+	
+	
 		
-		renderer.Draw(va, ib, shader);
+
+	
 
 	//	glEnableVertexAttribArray(0);
 		//glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
@@ -156,10 +175,14 @@ int main(void)
 		//	static float f = 0.0f;
 		//	static int counter = 0;
 		//	ImGui::Text("Hello, world!");                           // Display some text (you can use a format string too)
-			ImGui::SliderFloat3("Translation", &translation.x, 0.0f, 960.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
-		
+			ImGui::SliderFloat3("Translation A", &translationA.x, 0.0f, 960.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
+			ImGui::SliderFloat3("Translation B", &translationB.x, 0.0f, 960.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
+
 
 			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+		
+
+		
 		}
 
 		ImGui::Render();
